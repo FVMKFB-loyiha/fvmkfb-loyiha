6eabd86a4acc608e8906fe1b147cbb8f215426be
unknown <asadbekx405@gmail.com>
2024-11-25 20:41:22 +0300

    init commit

--- src/model/core/index.js
+++ src/model/core/index.js
@@ -3,3 +3,4 @@ import userModel from "../../model/core/user/user.model.js";
 import user_taskModel from "../../model/core/user_task/user_task.model.js";
 
 export { tasksModel, userModel, user_taskModel };
+
