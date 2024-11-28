e09dacadd992e3c5989a6bb6be4fea21d6b13279
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-11-28 21:36:19 +0500

    fixed some erro

--- src/common/database/sequelize.js
+++ src/common/database/sequelize.js
@@ -1,27 +1,15 @@
 import { Sequelize } from "sequelize";
 import getDotEnv from "../config/dotenv.config.js";
 
-const dbName = getDotEnv("DATABASE_NAME");
-const dbUser = getDotEnv("DATABASE_USER");
-const dbPass = getDotEnv("DATABASE_PASSWORD")?.toString(); // Ensure password is a string
-const dbHost = getDotEnv("DATABASE_HOST");
-const dbPort = parseInt(getDotEnv("DATABASE_PORT"));
-
-if (!dbName || !dbUser || !dbPass || !dbHost || !dbPort) {
-  throw new Error("Missing required database configuration");
-}
-
-const sequelize = new Sequelize(dbName, dbUser, dbPass, {
-  host: dbHost,
-  port: dbPort,
-  dialect: "postgres",
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+const sequelize = new Sequelize(
+  getDotEnv("DATABASE_NAME"),
+  getDotEnv("DATABASE_USER"),
+  getDotEnv("DATABASE_PASSWORD"),
+  {
+    host: getDotEnv("DATABASE_HOST"),
+    dialect: "postgres",
+    port: parseInt(getDotEnv("DATABASE_PORT")),
   }
-});
+);
 
 export default sequelize;
