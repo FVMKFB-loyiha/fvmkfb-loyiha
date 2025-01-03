c623a9437bd75b4e3201bb286edecb554e391e2a
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-03 16:38:45 +0500

    changed cores setting

--- src/app.js
+++ src/app.js
@@ -61,12 +61,11 @@ app.use("/user_task", userTaskRouter);
 import cors from "cors";
 app.use(
   cors({
-    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
+    optionsSuccessStatus: 200,
+    origin: "*",
   })
 );
-app.use(cors());
 
 // Swagger API documentationni ulash
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
@@ -85,14 +84,14 @@ app.use(
         bearerAuth: {
           name: "bearerAuth",
           schema: {
-            type: 'apiKey',
-            in: 'header',
-            name: 'Authorization',
-            description: "JWT token kiriting"
-          }
-        }
-      }
-    }
+            type: "apiKey",
+            in: "header",
+            name: "Authorization",
+            description: "JWT token kiriting",
+          },
+        },
+      },
+    },
   })
 );
 // app.post("/send-task", authenticateToken, upload.single('file'), async (req, res) => {
